b1c7f2bd276b7f11f9bfd5ddd542c6575a85e4bc
Reinaldo Ramos <masterrey@gmail.com>
2020-08-28 21:44:16 -0300

    Personagem completo
    movimento de camera , mira tiro ,passos etc

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -8,6 +8,8 @@ public class PlayerMove : MonoBehaviour
     public Animator anim;
     Vector3 mov;
     GameObject gamecamera;
+    public GameObject cameraAim;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +20,39 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         mov = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        cameraAim.transform.forward = gamecamera.transform.forward;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetBool("Shoot", true);
+
+        }
+        else
+        {
+            anim.SetBool("Shoot", false);
+        }
+
     }
 
     private void FixedUpdate()
     {
-        rdb.velocity = gamecamera.transform.TransformDirection(mov*10);
+        Vector3 cameraRelativeMov = gamecamera.transform.TransformDirection(mov); //movimento relativo a camera
+        rdb.velocity = new Vector3(cameraRelativeMov.x*4, rdb.velocity.y, cameraRelativeMov.z*4); //mantenho o y como velocidade
+        float radtogo = Vector3.Dot(transform.forward, -gamecamera.transform.right)*5;
+        transform.Rotate(0, radtogo, 0);
+        Vector3 locVel = transform.InverseTransformDirection(rdb.velocity).normalized;
 
-        transform.forward = gamecamera.transform.forward;
 
-        anim.SetFloat("Walk", rdb.velocity.magnitude);
+
+        anim.SetFloat("Walk", locVel.z);
+        anim.SetFloat("SideWalk", locVel.x+ radtogo);
+        anim.SetFloat("Speed", rdb.velocity.magnitude + Mathf.Abs(radtogo));
+    }
+    
+    
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+       anim.SetBoneLocalRotation(HumanBodyBones.Spine,cameraAim.transform.localRotation);
     }
 }
